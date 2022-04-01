d5c9cc9aba105c27a6bfdae4aab631de3c12bdf4
Joshua Vázquez Marú <44629814+JoshuaVM95@users.noreply.github.com>
2022-03-31 21:44:33 -0600

    refactor(github): create gh-pages-push script (#5)

---
+++ scripts/gh-pages-push.sh
@@ -0,0 +1,15 @@
+mkdir ~/.ssh
+echo "${{ secrets.PRIVATE_KEY }}" > ~/.ssh/id_ed25519
+sudo chmod 600 ~/.ssh/id_ed25519
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_ed25519
+cd build
+git init
+git config user.name github-actions
+git config user.email github-actions@github.com
+git remote add origin git@github.com:JoshuaVM95/JoshuaVM95.github.io.git
+git branch -M github-pages
+git add .
+git commit -m "Update gh-pages"
+git push -u --force origin github-pages
+rm -rf ~/.ssh
