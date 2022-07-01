354d5f0cb60461acf5ae1d5c1fc3e3bfba3b03ad
JoshuaVM95 <joshuavm95@gmail.com>
2022-06-30 21:21:33 -0500

    dependencies(react): update react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import "index.css";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
 
-render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement); // createRoot(container!) if you use TypeScript
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
